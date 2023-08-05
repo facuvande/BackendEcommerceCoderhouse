dbe215a161f078f8f7f1430605ebba607abf456c
Facundo Vandecaveye <facundovs84@hotmail.com.ar>
2023-08-05 15:46:54 -0300

    Ahora al crear un usuario con github se crea un carrito

--- Server/config/strategies/github.strategy.js
+++ Server/config/strategies/github.strategy.js
@@ -2,6 +2,12 @@ import passport from "passport";
 import { Strategy as GithubStrategy } from "passport-github2";
 import usersModel from "../../dao/models/users.model.js";
 import config from "../../config.js";
+import CartsFactory from "../../dao/Factorys/carts.factory.js";
+
+
+const Carts = await CartsFactory.getDao()
+const CartService = new Carts()
+
 
 export function githubStrategy(){
     passport.use('github', new GithubStrategy({
@@ -13,12 +19,16 @@ export function githubStrategy(){
             const email = profile._json.email;
             const user = await usersModel.findOne({email});
             if(!user){
+
+                const cartCreate = await CartService.create()
+
                 const newUser = await usersModel.create({
                     email,
                     firstName: profile._json.name,
                     lastName: '-',
                     password: '-',
                     age: 18,
+                    cart: cartCreate,
                     role: 'USER',
                 })
                 return done(null, newUser)
